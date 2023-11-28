99d7d15049c1fe3cfeb9de011ea72d89ac9b4d7c
bl <bouchkhaf@gmail.com>
2023-11-28 12:24:12 +0100

    commit step4_tp2

--- src/test/java/TextEditorTest.java
+++ src/test/java/TextEditorTest.java
@@ -1,2 +1,44 @@
+import fr.einfolearning.tp2.metiers.EmacsKillRing;
+import fr.einfolearning.tp2.metiers.TextBuffer;
+import fr.einfolearning.tp2.metiers.TextEditor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class TextEditorTest {
+    /*
+    @BeforeEach
+    public void init(){
+        TextBuffer buffer = mock(TextBuffer.class);
+        EmacsKillRing emacsKillRing = mock(EmacsKillRing.class);
+    }*/
+    /*
+    @Test
+    public void yankPopTest(){
+        EmacsKillRing emacsKillRing = mock(EmacsKillRing.class);
+        TextBuffer buffer = mock(TextBuffer.class);
+        // Arrange
+        when(emacsKillRing.isEmpty().theReturn(null));
+        IllegalAccessException iae = Assertions.assertThrows(IllegalAccessException.class, ()->  )
+*/
+
+
+
+
+
+        // Act
+
+        // Assert
+
+
+    }
 }
